1bee3591b610378053e38b0ed669cd4a8156fdab
Jayarathna18 <77055853+Jayarathna18@users.noreply.github.com>
2022-09-06 14:51:52 +0530

    Add files

---
+++ pages/componets/navbar.php
@@ -0,0 +1,69 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <title></title>
+
+</head>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"> Activity</a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Grammar</a></li>
+                <li><a class="dropdown-item" href="#"> Listening </a></li>
+                <li><a class="dropdown-item" href="#">Speaking</a></li>
+                <li><a class="dropdown-item" href="#">Writing</a></li
+
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+
+    </ul>
+
+        </div>
+
+            <div class="col-2">
+                <ul class="nav nav-tabs">
+
+
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-secondary  " href="#" tabindex="-1" aria-disabled="true">Log In</a>
+                    </li>
+
+                    <li class="nav-item bg-dark " style="float: right;">
+                        <a class="nav-link btn btn-secondary  text-light " href="#" tabindex="-1" aria-disabled="true">Register</a>
+                    </li>
+                </ul>
+
+        </div>
+    </div>
+</div>
+
+
+</html>
+
+
+
